7ca4e19791c6d627c2156b959a5bb229a5a52489
Kyle Lin <linkyle0924@gmail.com>
2024-08-15 17:29:54 -0700

    Create Stack

--- app/mainContent/Home.tsx
+++ app/mainContent/Home.tsx
@@ -2,20 +2,60 @@ import { StyleSheet } from 'react-native';
 
 import { Image, View, Text } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { useNavigation } from '@react-navigation/native'; // Import the hook
+
 interface MainContentProps {
   phase: string;
 }
 
-const MainContent: React.FC<MainContentProps> = ({ phase }) => {
+export const MainContent: React.FC<MainContentProps> = ({ phase }) => {
 
+  const navigation = useNavigation(); 
+
+  return(
+    <View>
+
+    </View>
+  )
+}
+
+export function Home() {
   return (
-    <View style = {styles.screen}>
-      <Image source={require('../../assets/images/maggy.png')} />
+    <View>
+      <Image source={require('../../assets/images/maggy.png')} style = {styles.image}/>
+    </View>
+  )
+}
+
+export function Preparation() {
+  return (
+    <View>
+      <Text>Preparation Page</Text>
     </View>
   );
 }
 
-export default MainContent;
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator screenOptions={{
+        headerStyle: { backgroundColor: 'rgb(193, 199, 195)'},
+      }
+        }>
+        <Stack.Screen name="Cat Adoption App" component={Home} />
+        <Stack.Screen name="Cat Adoption App: Preparation" component={Preparation} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MyStack;
 
 const styles = StyleSheet.create({
   screen: {
@@ -23,4 +63,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  image: {
+    height: 500, 
+    width: 400,
+  }
 });
