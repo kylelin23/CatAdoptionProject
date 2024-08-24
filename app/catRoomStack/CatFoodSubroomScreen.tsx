76622f334c362b5e293fea6e4cf5b037accd1c12
Donald Wong <donaldkwong@gmail.com>
2024-08-23 18:45:01 -0700

    Show content for cat food room

--- app/catRoomStack/CatFoodSubroomScreen.tsx
+++ app/catRoomStack/CatFoodSubroomScreen.tsx
@@ -2,13 +2,18 @@ import React, { useContext } from 'react';
 import { View, Text } from 'react-native'
 
 import PhaseContext, { PhaseContextType } from '@/context/PhaseContext';
+import { content, Subroom } from '@/constants/content';
 
 export const CatFoodSubroomScreen: React.FC = () => {
   const { phase } = useContext<PhaseContextType>(PhaseContext);
+  const subroomContent = phase ? content[phase][Subroom.food] : { thought: '', messages: [] };
 
   return (
     <View>
       <Text>Cat food subroom screen with phase: {phase}</Text>
+      <Text>Thought: {subroomContent?.thought}</Text>
+
+      {subroomContent?.messages.map((message, index) => <Text key={index}>Message: {message}</Text>)}
     </View>
   );
 }
